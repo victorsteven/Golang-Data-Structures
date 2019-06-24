ed6726ad3eb8bb1f19a599e8965d9636374aed6a
steven victor <chikodi543@gmail.com>
2019-06-24 09:23:31 +0100

    Initial commit

---
+++ array/01_package_scope_array/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var integerArray [5]int
+
+func main() {
+
+	integerArray[0] = 10
+	integerArray[1] = 20
+	integerArray[2] = 30
+	integerArray[3] = 40
+	integerArray[4] = 50
+
+	fmt.Println(integerArray)
+
+}
